123b212dededa95bc865fd6bc40e0135fe585176
Mastermindx33 <mastermindx33@gmail.com>
2021-09-13 10:59:13 +0530

    __init__Scope8x.js__

---
+++ PyScripts/Scope8x.js
@@ -0,0 +1,11 @@
+// Auto Zoom on Loading
+
+<script type="text/javascript">
+        function zoom() {
+            document.body.style.zoom = "300%" 
+        }
+</script>
+
+<body onload="zoom()">
+<h6>Foo Bar</h6>
+</body>
